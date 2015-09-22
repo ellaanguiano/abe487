782dffcaddc54d86310a27c34299dd688668b594
Ella Anguiano <ellaanguiano@email.arizona.edu>
2015-09-22 10:49:54 -0700

    hw3

--- hw3/samemod.pl
+++ hw3/samemod.pl
@@ -1,16 +1,17 @@
 
 #! /usr/bin/perl
-# file: reorder.pl
+# file: same.pl
 use strict;
 use warnings;
 
 my $string1 = shift(@ARGV);
 my $string2 = shift(@ARGV);
 
+
 if(! defined($string1) or ! defined($string2)) {
 	print "I need two strings, yo. \n" ;
-	} 
-	elsif ($string1 eq $string2) {
+} 
+	elsif (lc($string1) eq lc($string2)) {
 		print "same \n";
 	}
 	else {
